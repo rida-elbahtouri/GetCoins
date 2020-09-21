83c790585b8e34853b4c18b5b2f3c776287a420b
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-09-21 17:31:50 +0100

    make the cowboy fire

--- src/Scenes/WorldScene.js
+++ src/Scenes/WorldScene.js
@@ -9,7 +9,9 @@ export default class WorldScene extends Phaser.Scene {
     create () {    
     //map
         this.add.image(400,300,'bg')
-
+    this.tick =  this.time.now;
+    this.shouting = this.time.now
+    console.log(this.time.now)
     this.platforms = this.physics.add.staticGroup();
     this.platforms.create(400, 568, 'map').setScale(10,0.5).refreshBody();
     this.platforms.create(600, 400, 'map').setScale(3,0.2).refreshBody();
@@ -21,23 +23,35 @@ export default class WorldScene extends Phaser.Scene {
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
 
-        this.bullets = this.physics.add.sprite(100,500,'bullets')
-        this.bullets.setCollideWorldBounds(true);
+    // this.bullets = this.add.group()
+    // this.createBullet(this.bullets)
     this.cowboy = this.physics.add.sprite(780, 400, 'cowboy',{frame:2});
     this.cowboy.flipX=true
     this.cowboy.setBounce(0.2);
     this.cowboy.setCollideWorldBounds(true);
 
+    
     this.cameras.main.setBounds(0, 0, 800, 400);
     this.cameras.main.startFollow(this.player);
     this.cameras.main.roundPixels = true;
+    this.anims.create({
+        key:'fire',
+        frames:this.anims.generateFrameNumbers('bullets',{
+            frames:[96,97,98,99]
+        }),
+        
+        frameRate: 10,
+        repeat: 10,
+        setXY: { x: 780, y: 400, stepX: 10,stepY:20 }
+    })
     this.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('player', {
             frames: [3, 7, 11, 15],
           }),
         frameRate: 10,
-        repeat: -1
+        repeat: 10,
+        setXY: { x: 780, y: 400, stepX: 0 }
     });
 
     this.anims.create({
@@ -68,7 +82,9 @@ export default class WorldScene extends Phaser.Scene {
         setScale: { x: 0.1, y: 0.1}
     });
     console.log(this.coins)
+    this.bullets = this.physics.add.group()
     this.cowboy.play('shoot')
+    //this.bullets.play('fire')
     this.coins.children.iterate(function (child) {
 
         //  Give each coin a slightly different bounce
@@ -84,9 +100,32 @@ this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#f
     //  Checks to see if the this.player overlaps with any of the coins, if he does call the collectStar function
     this.physics.add.overlap(this.player, this.coins, this.collectCoins, null, this);
     this.physics.add.overlap(this.cowboy,this.player, this.die, null, this);
-    
+   
+    this.bullets = this.add.group()
+
+   
 }
 
+createBullet(){
+    
+    this.bullets.createMultiple({ 
+        key:'bullets',frame:96,setXY:{x:780,y:500}
+    })
+    this.bullets.children.iterate((sp)=>{
+        console.log(sp)
+    })
+    this.bullets.children.iterate((child)=> {
+        this.physics.add.collider(this.bullets, this.platforms);
+        //  Give each coin a slightly different bounce
+        child.play('fire')
+        
+            this.physics.add.overlap( child,this.player, this.die, null, this);
+
+    })
+    //this.bullets = this.physics.add.sprite(100,500,'bullets',96).play('fire')
+
+    
+}
 collectCoins (player, coin)
 {
     coin.disableBody(true, true);
@@ -100,7 +139,7 @@ collectCoins (player, coin)
     {
         //  A new batch of .coins to collect
         this.coins.children.iterate(function (child) {
-
+            
             child.enableBody(true, child.x, 0, true, true);
 
         });
@@ -114,7 +153,14 @@ die(){
     this.scoreText.setText('Score: ' + this.score);
     this.player.disableBody(true, true)
 }
+
+shoot(){
+    this.bullets.x += (-2)
+    this.bullets.y+= (-2)
+}
+
 update(){
+    
     let cursors = this.input.keyboard.createCursorKeys();
     if (cursors.left.isDown)
     {
@@ -139,6 +185,21 @@ update(){
     {
         this.player.setVelocityY(-330);
     }
+    this.shoot()
+    this.physics.add.overlap( this.bullets,this.player, this.die, null, this);
+    if (this.time.now - this.tick > 1000) {
+        this.createBullet(this.bullets)
+         this.tick = this.time.now;
+        }
+        if (this.time.now - this.shouting > 10) {
+            this.bullets.children.iterate( (child)=> {
+                child.x += Phaser.Math.Between(-4, -2)
+                child.y+= Phaser.Math.Between(-4, 3)
+               
+            })
+          
+             this.shouting = this.time.now;
+            }
 }
 
   };
